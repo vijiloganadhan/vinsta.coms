2e7433d2091530e4260d2bf38ad12802916b7f4d
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-09-01 19:21:48 +0530

    new vinsta

--- insta_icom/insta_icom/settings.py
+++ insta_icom/insta_icom/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q2r!6eysc(xq=htb9@jchs9!d(#3)vs)=_6r+k7t#&k92f+ox^'
+SECRET_KEY =os.environ.get("SECRET_KEY","your secert key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST","").split(",")
+
 
 
 # Application definition
@@ -81,7 +82,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+import dj_django_url
+DATABASES["default"]=dj_django_url.parse(os.environ.get("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
