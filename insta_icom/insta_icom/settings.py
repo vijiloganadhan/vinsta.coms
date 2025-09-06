59c5f6187ad00d91d3395c12b32f125a4b62a310
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-09-06 07:22:46 +0530

    new

--- insta_icom/insta_icom/settings.py
+++ insta_icom/insta_icom/settings.py
@@ -86,9 +86,8 @@ DATABASES = {
     }
 }
 import dj_database_url
-DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+DATABASES = {"default": dj_database_url.config(default=os.environ["DATABASE_URL"])}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
